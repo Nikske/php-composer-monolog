17208dc85eec07fb2a84c914a8659c63e40de9d6
Nikske <wolfvdd@gmail.com>
2020-03-02 15:38:14 +0100

    switch case

--- index.php
+++ index.php
@@ -7,12 +7,49 @@ error_reporting(E_ALL);
 require 'vendor/autoload.php';
 use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
+use Monolog\Handler\BrowserConsoleHandler;
 
-// create a log channel
-$log = new Logger('test');
-$log->pushHandler(new StreamHandler(__DIR__.'/logs/warning.log', Logger::DEBUG));
+// Logger
+$log = new Logger('Logger');
+$input = $_GET['message'];
+switch($_GET['type']) {
+    case 'DEBUG':
+        $log->pushHandler(new StreamHandler(__DIR__.'/logs/info.log', Logger::DEBUG));
+        $log->pushHandler(new BrowserConsoleHandler(Logger::DEBUG));
+        $log->debug($input);
+        break;
+    case 'INFO':
+        $log->pushHandler(new StreamHandler(__DIR__.'/logs/info.log', Logger::INFO));
+        $log->pushHandler(new BrowserConsoleHandler(Logger::INFO));
+        $log->info($input);
+        break;
+    case 'NOTICE':
+        $log->pushHandler(new StreamHandler(__DIR__.'/logs/info.log', Logger::NOTICE));
+        $log->pushHandler(new BrowserConsoleHandler(Logger::NOTICE));
+        $log->notice($input);
+        break;
+    case 'WARNING':
+        $log->pushHandler(new StreamHandler(__DIR__.'/logs/warning.log', Logger::WARNING));
+        $log->warning($input);
+        break;
+    case 'ERROR':
+        $log->pushHandler(new StreamHandler(__DIR__.'/logs/warning.log', Logger::ERROR));
+        $log->error($input);
+        break;
+    case 'CRITICAL':
+        $log->pushHandler(new StreamHandler(__DIR__.'/logs/warning.log', Logger::CRITICAL));
+        $log->critical($input);
+        break;
+    case 'ALERT':
+        $log->pushHandler(new StreamHandler(__DIR__.'/logs/warning.log', Logger::ALERT));
+        $log->alert($input);
+        break;
+    case 'EMERGENCY':
+        $log->pushHandler(new StreamHandler(__DIR__.'/logs/emergency.log', Logger::EMERGENCY));
+        $log->emergency($input);
+        break;
+}
 
-// add records to the log
-$log->info($_GET['message']);
+
 
 require 'buttons.html';
